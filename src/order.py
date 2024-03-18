237dc217a4dc55987044749c555af972b2874ff8
Daniel Ryan <31455292+DNRyan@users.noreply.github.com>
2024-03-18 17:06:17 +0000

    remove order methods

--- src/order.py
+++ src/order.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass
 
 from src.address import Address
 from src.product import Product
-from src.warehouse import Warehouse
-from src.shipping import ShippingCalculator
 
 
 @dataclass
@@ -16,20 +14,3 @@ class Item:
 class Order:
     shipping_address: Address
     items: list[Item]
-
-    def add_item(self, warehouse: Warehouse, item: Item) -> None:
-        available_stock = warehouse.get_product_stock(product=item.product)
-        if available_stock < item.quantity:
-            raise ValueError("Required stock not available for product")
-
-        self.items.append(item)
-        warehouse.reduce_product_stock(
-            product=item.product, amount_to_remove=item.quantity
-        )
-
-    def calculate_total(self, shipping_calculator: ShippingCalculator) -> float:
-        order_total = sum([item.quantity * item.product.price for item in self.items])
-        shipping_cost = shipping_calculator.calculate_shipping(
-            self.shipping_address.country.value, order_total
-        )
-        return order_total + shipping_cost
